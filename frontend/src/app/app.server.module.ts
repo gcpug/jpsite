c3731c1bbd9d2a521f9eb6a2d9e593bc2072d5d5
pyrovers <pyroverse.392@gmail.com>
2021-02-08 17:26:56 +0900

    style.css の SSR 化 refs #21

--- frontend/src/app/app.server.module.ts
+++ frontend/src/app/app.server.module.ts
@@ -3,12 +3,11 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { InlineStyleModule } from './inline-style/inline-style.module';
+import { InlineStyleComponent } from './inline-style/inline-style.component';
 
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
-  bootstrap: [AppComponent],
+  imports: [AppModule, ServerModule, InlineStyleModule],
+  bootstrap: [AppComponent, InlineStyleComponent],
 })
 export class AppServerModule {}
---
+++ frontend/src/app/inline-style/inline-style.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-inline-style',
+  template: '',
+  styleUrls: ['./inline-style.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class InlineStyleComponent {}
